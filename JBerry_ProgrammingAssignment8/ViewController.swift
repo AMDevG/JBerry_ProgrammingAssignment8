9b85f60eb0e196b338bcd8b838c9afd450efa79c
AMDevG <jeberry308@gmail.com>
2017-03-08 20:18:06 -0600

    Initial Commit

---
+++ JBerry_ProgrammingAssignment8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JBerry_ProgrammingAssignment8
+//
+//  Created by John Berry on 3/8/17.
+//  Copyright © 2017 dev G Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
